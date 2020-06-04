d61f1729e8e9268de6aa5eb089b9bc7779cadfcf
pabloqb2000 <qb.pablo.2000@gmail.com>
2020-06-04 13:40:41 +0200

    Done coding

--- Draggable/drag_circle.js
+++ Draggable/drag_circle.js
@@ -6,23 +6,35 @@ class DragCircle extends DragElement{
      * @param onDrag Action to perform when the object is dragged
      * @param onClick Action to perform when the object is clicked
      */
-    constructor(pos, r=15, onDrag=null, onClick=null) {
+    constructor(pos, r=15, color, onDrag=null, onClick=null) {
         super(pos, true, true, onClick == null, onDrag, onClick);
         this.r = r;
+        this.color = color;
+        this.offset = createVector(13/24*width, height/2);
     }
 
     /**
      * Draw the object
      */
     draw() {
-        fill(this.highlighted ? 200 : 230);
+        fill(this.highlighted ? 230 : this.color);
         ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
+    }
+
+    /**
+     * If the mouse is over the element
+     * dragg it to the mouse position
+     * and perform the on drag action
+     */
+    dragged() {
+        this.pos = createVector(mouseX, mouseY).sub(this.offset);
+        if(this.onDrag != null) this.onDrag();
     }
 
     /**
      * Check if mouse is over the object
      */
     mouseIsOver() {
-        return createVector(mouseX, mouseY).dist(this.pos) <= this.r;
+        return createVector(mouseX, mouseY).sub(this.offset).dist(this.pos) <= this.r + 10;
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,46 +1,63 @@
-let sld, btn, tggl, cPicker, optBox;
-let circle, rectangle;
+let sld, hideBtn, stopBtn;
+let points;
+let startPt;
+let lastDrawn;
+let iterations = 250;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight+5);
 	background(32);
 
 	// Create UI elements
-	sld = new Slider(start=0, end=255, value=32, 0, 0, width/12, height/60, null, "Background");
-	btn = new Button(x=0, y=0, width/12, height/30, "Reset", resetValue);
-	
-	tggl = new ToggleButton(0,0,width/12,height/30,"Discrete", discretice);
-	cPicker = new ColorPicker(0,0, width/12, height/30, null, "Color 1");
+	sld = new Slider(start=3, end=24, value=3, 0, 0, width/12, height/60, 1, "Points", true, 0, resetPoints);
+	hideBtn = new ToggleButton(0,0, width/12, height/30, "Hide pts", reDrawPoints);
+	stopBtn = new ToggleButton(0,0, width/12, height/30, "Stop");
 
-	optBox = new OptionsBox(["Option1", "Option2", "Option3"], 20, () => console.log("Option changed"));
-
-	// Add two draggable elements
-	circle = new DragCircle(createVector(width/2 + 40, height/2 + 40), 20);
-	rectangle = new DragRect(createVector(width/2 - 40, height/2 - 40), 40, 40);
+	startPt = new DragCircle(createVector(0,0), 3, color(227, 103, 86), reDrawPoints)
 
 	// Start UI
 	UI.tableWidth = 1;
 	UI.tableHeight = 100;
 	UI.distrubute();
+
+	resetPoints();
 }
 
 function draw() {
 	// Draw UI and draggable elements
-	background(sld.value);
+	fill(32);
+	noStroke();
+	rect(sld.x, sld.y, sld.width, sld.height);
 	UI.update();
 	UI.draw();
-	Drag.update();
-	Drag.draw();
 
-	
 	translate(13/24*width, height/2);
-	scale(1,-1);
+	Drag.update();
+	if(!hideBtn.active) Drag.draw();
+
+	if(!stopBtn.active){
+		stroke(230, 100);
+		strokeWeight(1);
+		for(let i = 0; i < iterations*sld.value; i++) {
+			lastDrawn = p5.Vector.add(lastDrawn, random(points).pos).div(2);
+			point(lastDrawn.x, lastDrawn.y);
+		}
+	}
 }
 
-function resetValue() {
-	sld.value = 32;
+function resetPoints() {
+	points = [];
+	Drag.elements = [startPt];
+	Drag.selected = null;
+	for(let i = 0; i < sld.value; i++) {
+		let a = 2*PI*i/sld.value;
+		points.push(new DragCircle(createVector(width/4 * sin(a), width/4 * cos(a)), 3, color(86, 210, 227), reDrawPoints))
+	}
+
+	reDrawPoints();
 }
 
-function discretice() {
-	sld.step = (sld.step == null ? 20 : null);
+function reDrawPoints() {
+	background(32);
+	lastDrawn = startPt.pos.copy();
 }
